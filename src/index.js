ff4d5a2592375f4f5ab05523652253d1b60a7b64
Naimerith <naimerith89@gmail.com>
2021-10-17 16:59:15 -0500

    Se agrego la funcion de los numeros pares y los estilos css de la app web

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
+
 import validator from './validator.js';
 
 console.log(validator);
+
+let tarjeta = document.getElementById('tarjeta'); 
+const btnClick = document.getElementById('btnClick');
+
+
+//Esta funcion va a llamar a al metodo isValid cuando se de click en el boton 
+function validarTarjeta (){
+//declaro una variable que va a traer el valor de mi input (en este caso el numero de tarjeta)    
+    let digitos= tarjeta.value; 
+//declaro una variable resultado que va a llamar a mi funcion validator y va a recorrer el metodo isValid y le paso la variable digitos (en donde se encuentra el valor de mi tarjeta)
+    let resultado = validator.isValid(digitos); 
+
+//Hago la condicion de si mi resultado es igual a verdadero muestre un alert con tarjeta valida 
+    if (resultado == true){
+        alert("La tarjeta es valida");
+    }// sino muestra un alert con tarjeta no valida. 
+    else{
+        alert("La tarjeta no es valida");
+    }
+}
+
+//Es un evento que se debe ejecutar despues de la funcion validarTarjeta, el cual escucha al boton al dar click 
+btnClick.addEventListener ("click", validarTarjeta)
+
+    
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,29 @@
-const validator = {
-  // ...
-};
 
-export default validator;
+const validator = {
+    isValid: function (creditCardNumber){
+        let numberPosition = creditCardNumber.split("").reverse(); //Ingreso el numero de tarjeta en un array, lo reversa y lo separa en string
+        let sum = 0;
+
+        //i inicia en 0 y siempre que i sea menor que arrayInverted.length continua el bucle incrementado en 1
+        for(let i=0; i<numberPosition.length; i++){
+
+            //(%operador modulo: residuo) si al dividir i/2 el residuo es diferente a cero, entonces la posicion es impar. array comienza en cero sus posiciones por ende debe contarse. 
+            if (i%2 !=0){    
+                let newResult = (numberPosition[i]*2); // Multiplica posicion impar x2 
+
+                if(newResult >= 10){ //Si mi resultado de las posiciones impares es mayor o igual a 10 
+                    let digitsPosition = newResult.toString().split(""); //el resultado de las posiciones impares metelas es un nuevo array y separalo en string
+
+                    //suma entre si los digitos >=10 que estan en mi variable newResult
+                    let arr1 = digitsPosition[0].match(/\d+/g).map(Number); //Convierto en numero el string que esta en la posicion 0 
+                    let arr2 = digitsPosition[1].match(/\d+/g).map(Number); //Convierto en numero el string que esta en la posicion 1 
+                    let arrF = parseFloat(arr1)+ parseFloat(arr2); //sumo entre si los numeros de cada array 
+
+                    console.log(digitsPosition);
+                }
+            }
+        }
+    }
+}
+ 
+export default validator; 
