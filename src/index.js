484e6661c5f434397378fd6c626e8e63ad35d647
Naimerith <naimerith89@gmail.com>
2021-10-22 14:52:41 -0500

    Actualizando la funcionalidad del  proyecto

--- src/index.js
+++ src/index.js
@@ -3,28 +3,38 @@ import validator from './validator.js';
 
 console.log(validator);
 
-let tarjeta = document.getElementById('tarjeta'); 
 const btnClick = document.getElementById('btnClick');
 
 
 //Esta funcion va a llamar a al metodo isValid cuando se de click en el boton 
 function validarTarjeta (){
-//declaro una variable que va a traer el valor de mi input (en este caso el numero de tarjeta)    
-    let digitos= tarjeta.value; 
-//declaro una variable resultado que va a llamar a mi funcion validator y va a recorrer el metodo isValid y le paso la variable digitos (en donde se encuentra el valor de mi tarjeta)
-    let resultado = validator.isValid(digitos); 
+
+    let tarjeta = document.getElementById('tarjeta'); //obtengo el elemento id que esta en el documento 
+
+//Guardo en una variable que va a traer el valor de mi input ( var = tarjeta)    
+    let numberPosition= tarjeta.value; 
+//declaro una variable resultado que va a llamar a mi funcion validator y va a recorrer el metodo isValid
+    let resultado = validator.isValid(numberPosition); 
+
+//declaro una variable resultado que va a llamar a mi funcion validator y va a recorrer el metodo isValid
+    let enmascarar = validator.maskify(numberPosition);
 
 //Hago la condicion de si mi resultado es igual a verdadero muestre un alert con tarjeta valida 
     if (resultado == true){
         alert("La tarjeta es valida");
+        tarjeta.value = enmascarar; 
     }// sino muestra un alert con tarjeta no valida. 
     else{
-        alert("La tarjeta no es valida");
+        alert("La tarjeta es Invalida");
+        tarjeta.value = enmascarar; 
     }
 }
 
+
 //Es un evento que se debe ejecutar despues de la funcion validarTarjeta, el cual escucha al boton al dar click 
 btnClick.addEventListener ("click", validarTarjeta)
+
+
 
     
 
--- src/validator.js
+++ src/validator.js
@@ -1,29 +1,55 @@
-
+//VALIDAR EL NUMERO DE TARJETA DE CREDITO INGRESADO EN EL FORMULARIO (ALGORITMO)
 const validator = {
-    isValid: function (creditCardNumber){
-        let numberPosition = creditCardNumber.split("").reverse(); //Ingreso el numero de tarjeta en un array, lo reversa y lo separa en string
-        let sum = 0;
+    isValid: function (numberPosition){
+        numberPosition = numberPosition.split("").reverse(); //Ingreso el numero de tarjeta en un array, lo reversa y lo separa en string
+    
+        let suma = 0; // Guardo el resultado de las posiciones pares 
 
-        //i inicia en 0 y siempre que i sea menor que arrayInverted.length continua el bucle incrementado en 1
-        for(let i=0; i<numberPosition.length; i++){
+       //recorro cada posicion del numero de tarjeta de 1 en 1 
+        for (let i=0; i<numberPosition.length; i++){
+            //Si posicion i/2, su residuo es diferente a cero es impar 
+            if(i%2 !=0){
+                let result = parseInt(numberPosition[i])*2; //multiplico cada posicion por 2 (recuerda que el array comienza a contar en 0)
 
-            //(%operador modulo: residuo) si al dividir i/2 el residuo es diferente a cero, entonces la posicion es impar. array comienza en cero sus posiciones por ende debe contarse. 
-            if (i%2 !=0){    
-                let newResult = (numberPosition[i]*2); // Multiplica posicion impar x2 
+                //Si en mi resultado hay digitos >=10, convierto en string y los separo
+                if(result >=10){
+                    let parPosition = result.toString(). split("");
+                    let digitosTotal = 0; //guardo el resultado 
 
-                if(newResult >= 10){ //Si mi resultado de las posiciones impares es mayor o igual a 10 
-                    let digitsPosition = newResult.toString().split(""); //el resultado de las posiciones impares metelas es un nuevo array y separalo en string
+                    //Recorro los digitos de las posiciones pares 
+                    for(let j=0; j<parPosition.length; j++){
+                        digitosTotal = digitosTotal + parseInt(parPosition[j]); //Los convierto a numeros y sumo entre si 
+                    }
+                    // guardo el nuevo resultado de sumar los numeros entre si 
+                    result = digitosTotal; 
+                }   
+                suma = suma + result; // Ingreso el resultado en mi variable global para luego sumar con el resto de los digitos de la tarjeta 
 
-                    //suma entre si los digitos >=10 que estan en mi variable newResult
-                    let arr1 = digitsPosition[0].match(/\d+/g).map(Number); //Convierto en numero el string que esta en la posicion 0 
-                    let arr2 = digitsPosition[1].match(/\d+/g).map(Number); //Convierto en numero el string que esta en la posicion 1 
-                    let arrF = parseFloat(arr1)+ parseFloat(arr2); //sumo entre si los numeros de cada array 
-
-                    console.log(digitsPosition);
-                }
+                //Si no son mayores a 10 recorre el arreglo y guarda los digitos en la var suma 
+            } else{
+                suma = suma + parseInt(numberPosition[i]);
             }
         }
+        //Mi resultado final, sera el residuo de mi var suma entre 10 
+        let resultadoFinal = suma % 10; 
+
+        //Si mi resutado es igual a cero (no tiene residuo) la tarjeta es valida, sino es invalida. 
+            if(resultadoFinal ==0)
+            return true;
+            else
+            return false;
+            },
+
+// ENMASCARAR LOS ULTIMOS 4 DIGITOS DE LA TARJETA DE CREDITO 
+    maskify: function(numberPosition) {
+        let num = Array.from(numberPosition); //paso mi parametro a un arreglo con el metodo Array.from()
+
+        //El ciclo va a recorrer cada posición de una en una, y va a enmascar todos los digitos menos los ultimos 4 digitos
+              for (let i = 0; i <numberPosition.length -4; i++) {
+                num[i]="#"; 
+        }
+        return num.join("");  //retorna el numero de la tarjeta devuelto en string (.join)
     }
 }
- 
-export default validator; 
+
+export default validator;
