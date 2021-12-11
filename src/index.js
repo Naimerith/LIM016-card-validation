295606af9afbf878f9f5cf47551454560fce688c
Naimerith <naimerith89@gmail.com>
2021-12-11 15:39:37 -0500

    proyecto terminado

--- src/index.js
+++ src/index.js
@@ -1,39 +1,39 @@
 
 import validator from './validator.js';
 
-const btnClick = document.getElementById("btnClick"); 
+const btnClick = document.getElementById("btnClick");
 const formulario = document.getElementById("formulario");
 
 const divMensajeVal = document.createElement('div');
-    divMensajeVal.classList.add("tarjetaValida");
-    divMensajeVal.innerHTML = `✔ Su tarjeta fue Validada satisfactoriamente`;
-    document.getElementById ("valida").appendChild(divMensajeVal);
-    divMensajeVal.style.display = "none";
+divMensajeVal.classList.add("tarjetaValida");
+divMensajeVal.innerHTML = `✔ Su tarjeta fue Validada satisfactoriamente`;
+document.getElementById("valida").appendChild(divMensajeVal);
+divMensajeVal.style.display = "none";
 
 const divMensajeInv = document.createElement('div');
-    divMensajeInv.classList.add("tarjetaInvalida");
-    divMensajeInv.innerHTML = `✘ Tarjeta Invalida`;
-    document.getElementById ("invalida").appendChild(divMensajeInv);
-    divMensajeInv.style.display = "none";
+divMensajeInv.classList.add("tarjetaInvalida");
+divMensajeInv.innerHTML = `✘ Tarjeta Invalida`;
+document.getElementById("invalida").appendChild(divMensajeInv);
+divMensajeInv.style.display = "none";
 
 
 /* ----Rellenar el campo de mes-----
 Con un ciclo establecemos los meses del 1 al 12, declaro una variable que va a almacenar este resultado y creo el elemento option para traer su valor y reemplazarlo por el valor de i, con el appendChild ingreso estos valores dentro de mi etiqueta select a traves de su ID*/
-for(let i=1; i <= 12; i++){
-    let opcion = document.createElement("option"); 
-        opcion.value = i; 
-        opcion.innerText = i;
-        formulario.selectMes.appendChild(opcion);
+for (let i = 1; i <= 12; i++) {
+    let opcion = document.createElement("option");
+    opcion.value = i;
+    opcion.innerText = i;
+    formulario.selectMes.appendChild(opcion);
 }
 
 /*Rellenar el campo de año
 Declaro una variable para saber el año actual, la cual sera igual al año del sistema por el metodo para obtener el año completo. Lueg con el ciclo for cuento desde el año actual incremento en 1 hasta 10 años. Se repite el mismo procedimiento del mes solo se cambia el ID del año*/
 let yearActual = new Date().getFullYear();
-for (let j = yearActual; j <= yearActual + 10; j++){
-    let opcion = document.createElement("option"); 
-        opcion.value = j; 
-        opcion.innerText = j;
-        formulario.selectYear.appendChild(opcion);
+for (let j = yearActual; j <= yearActual + 10; j++) {
+    let opcion = document.createElement("option");
+    opcion.value = j;
+    opcion.innerText = j;
+    formulario.selectYear.appendChild(opcion);
 }
 
 /* --- Esta funcion va a llamar a a los metodos isValid y Maskify cuando se de click en el boton---
@@ -43,56 +43,56 @@ La variable enmascarar va a llamar a mi función Maskify
 La variable resultado va a llamar a mi función Isvalid
 Condicion; SI mi resultado es igual a verdadero muestre un alert con tarjeta valida, SINO la tarjeta es invalida - En ambos casos se ocultan todos los digitos menos los ultimos 4 digitos*/
 
-function validarTarjeta (){
-    let tarjeta = document.getElementById('tarjeta'); 
-    let numberPosition= tarjeta.value; 
-    
-    let enmascarar = validator.maskify(numberPosition); 
-    let resultado = validator.isValid(numberPosition); 
+function validarTarjeta() {
+    let tarjeta = document.getElementById('tarjeta');
+    let numberPosition = tarjeta.value;
 
-    tarjeta.value = enmascarar; 
+    let enmascarar = validator.maskify(numberPosition);
+    let resultado = validator.isValid(numberPosition);
 
-    if (resultado){
-       // alert("La tarjeta es valida");     
-       divMensajeVal.style.display = "block";
+    tarjeta.value = enmascarar;
+
+    if (resultado) {
+        // alert("La tarjeta es valida");     
+        divMensajeVal.style.display = "block";
     }
-    else{
+    else {
         //alert("La tarjeta es Invalida");
         divMensajeInv.style.display = "block";
     }
     //resultado?div1:div2; // Esto es un operador ternario 
 
-/*----Para duplicar los datos del formulario en el simulador (tarjeta)---- 
-Declaro una const que va a acceder al valor de mi ID en la tarjeta 
-Declaro otra variable que va a acceder al valor de mi ID en el formulario 
-Guardo en una variable, el valor de mi ID 
-Invoco a la variable que accedio al ID de mi formulario, le aplico la propiedad innerHtml, q va a leer el dato y lo va a asignar a la variable que accedio al ID de mi tarjeta. 
-*/
-let duplicate = document.getElementById("tarjeta-numero");
-duplicate.innerHTML = numberPosition; //Duplico el numero de la tarjeta en el simulador 
+    /*----Para duplicar los datos del formulario en el simulador (tarjeta)---- 
+    Declaro una const que va a acceder al valor de mi ID en la tarjeta 
+    Declaro otra variable que va a acceder al valor de mi ID en el formulario 
+    Guardo en una variable, el valor de mi ID 
+    Invoco a la variable que accedio al ID de mi formulario, le aplico la propiedad innerHtml, q va a leer el dato y lo va a asignar a la variable que accedio al ID de mi tarjeta. 
+    */
+    let duplicate = document.getElementById("tarjeta-numero");
+    duplicate.innerHTML = numberPosition; //Duplico el numero de la tarjeta en el simulador 
 
-const duplicateName = document.getElementById("tarjeta-name");
-let name = document.getElementById("name"); 
-let titular = name.value; 
-duplicateName.innerHTML = titular; 
+    const duplicateName = document.getElementById("tarjeta-name");
+    let name = document.getElementById("name");
+    let titular = name.value;
+    duplicateName.innerHTML = titular;
 
-let duplicateMes = document.getElementById("tarj-mes");
-let mes = document.getElementById("selectMes");
-let tarjMes = mes.value;
-duplicateMes.innerHTML = tarjMes;
+    let duplicateMes = document.getElementById("tarj-mes");
+    let mes = document.getElementById("selectMes");
+    let tarjMes = mes.value;
+    duplicateMes.innerHTML = tarjMes;
 
-let duplicateYear = document.getElementById("tarj-year");
-let year = document.getElementById("selectYear");
-let tarjYear = year.value;
-duplicateYear.innerHTML = tarjYear;
+    let duplicateYear = document.getElementById("tarj-year");
+    let year = document.getElementById("selectYear");
+    let tarjYear = year.value;
+    duplicateYear.innerHTML = tarjYear;
 
-    
-}  
+
+}
 
 /* -----Boton Validar-----
 Es un evento que se debe ejecutar despues de la funcion validarTarjeta, el cual escucha al boton al dar click */
 
-btnClick.addEventListener ("click", validarTarjeta); 
-    
+btnClick.addEventListener("click", validarTarjeta);
+
 
 
--- src/validator.js
+++ src/validator.js
@@ -1,46 +1,46 @@
 
 /* ***** Función para Validar Tarjeta ***** */
 const validator = {
-    isValid: function (numberPosition){
+    isValid: function (numberPosition) {
         numberPosition = numberPosition.split("").reverse(); //.split devuelve un nuevo array en reversa, separarando los valores en cadenas 
         let addAllDigits = 0; // Guardo el resultado de las posiciones pares 
 
-        for (let i=0; i<numberPosition.length; i++){        //recorro cada posicion del numero de tarjeta de 1 en 1 
-            if(i%2 !=0){             //Si posicion i/2, su residuo es diferente a cero esta en posicion par (recuerda que el array comienza en 0)        
-                let resultPositionEven = parseInt(numberPosition[i])*2; //multiplico cada posicion por 2 (recuerda que el array comienza a contar en 0)
-                
-                if(resultPositionEven >=10){                //Si en mi resultado hay digitos >=10, .split devuelve un nuevo array separando los digitos en string (toString)
-                    let digitsTwo = resultPositionEven.toString(). split(""); 
+        for (let i = 0; i < numberPosition.length; i++) {        //recorro cada posicion del numero de tarjeta de 1 en 1 
+            if (i % 2 != 0) {             //Si posicion i/2, su residuo es diferente a cero esta en posicion par (recuerda que el array comienza en 0)        
+                let resultPositionEven = parseInt(numberPosition[i]) * 2; //multiplico cada posicion por 2 (recuerda que el array comienza a contar en 0)
+
+                if (resultPositionEven >= 10) {                //Si en mi resultado hay digitos >=10, .split devuelve un nuevo array separando los digitos en string (toString)
+                    let digitsTwo = resultPositionEven.toString().split("");
                     let totalDigits = 0; //inicializo en cero para guardar datos posteriores 
 
-                    for(let j=0; j<digitsTwo.length; j++){                     //Recorro los digitos de las posiciones pares 
+                    for (let j = 0; j < digitsTwo.length; j++) {                     //Recorro los digitos de las posiciones pares 
                         totalDigits += parseInt(digitsTwo[j]); //Los convierto a numeros y sumo entre si los dos digitos
                     }
                     resultPositionEven = totalDigits; //Agrego el total de sumar los dos digitos entre si al resto de los digitos de la posicion par  
-                }   
+                }
                 addAllDigits = addAllDigits + resultPositionEven;  //sumo todos los digitos de la posicion par 
-                
-            } else{  //Si no son mayores a 10 recorre el arreglo y guarda los digitos en la var suma 
+
+            } else {  //Si no son mayores a 10 recorre el arreglo y guarda los digitos en la var suma 
                 addAllDigits = addAllDigits + parseInt(numberPosition[i]);
             }
         }
         let finalScore = addAllDigits % 10; //Mi resultado final, sera el residuo de mi var suma de todos los digitos entre 10 
-        
+
         //Si mi resutado es igual a cero (no tiene residuo) la tarjeta es valida, sino es invalida. 
-            if(finalScore ==0)
+        if (finalScore == 0)
             return true;
-            else
+        else
             return false;
-            },
+    },
 
 
-// ENMASCARAR LOS ULTIMOS 4 DIGITOS DE LA TARJETA DE CREDITO 
-    maskify: function(numberPosition) {
+    // ENMASCARAR LOS ULTIMOS 4 DIGITOS DE LA TARJETA DE CREDITO 
+    maskify: function (numberPosition) {
         let num = Array.from(numberPosition); //paso mi parametro a un arreglo con el metodo Array.from()
 
         //El ciclo va a recorrer cada posición de una en una, y va a enmascar todos los digitos menos los ultimos 4 digitos
-              for (let i = 0; i <numberPosition.length -4; i++) {
-                num[i]="#"; 
+        for (let i = 0; i < numberPosition.length - 4; i++) {
+            num[i] = "#";
         }
         return num.join("");  //retorna el numero de la tarjeta devuelto en string (.join)*/
     }
